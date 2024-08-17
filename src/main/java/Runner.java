a1dcd22d5a8c37841c7c6ad65fd9fd4414732d6d
Thai Vo <dythirk@gmail.com>
2024-08-17 12:09:50 +0000

    Final StaticKeyword

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -22,7 +22,6 @@ public class Runner {
      * @param in the String value you should change global_var to in.
      */
     public void changeClassVar(String in){
-        global_var = in;
 
     }
 }
